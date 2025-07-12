55cd9ca132cb0bf869b7817030f830437e97e709
Dayana <baltodanodayana24@gmail.com>
2025-07-12 10:33:09 -0600

    Inicializar Firebase en Flutter e instalar paquetes

---
+++ README.md
@@ -0,0 +1,3 @@
+# test_dragon
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/test_dragon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_dragon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
